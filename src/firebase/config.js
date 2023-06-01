1ac63c8e2567511e6e655f5fad27d3cd80271430
Romina Galarza <romina.celeste.gza@gmail.com>
2023-06-01 14:19:42 -0300

    modificado firebase/.config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,19 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration -  import.meta en lugar de process en React + Vite en lugar de process
+// Your web app's Firebase configuration 
+// import.meta.env.VITE_APP_apiKey en lugar de process, y lo demas, en React + Vite en lugar de process
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_apiKey,
-  authDomain: import.meta.env.VITE_APP_authDomain,
-  projectId: import.meta.env.VITE_APP_projectId,
-  storageBucket: import.meta.env.VITE_APP_storageBucket,
-  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
-  appId: import.meta.env.VITE_APP_appId
+
+  apiKey: "AIzaSyCD-ewFbh-mr1uCH80C4dIRwM0UImfwr2Q",
+  authDomain: "peanutgo-c13f7.firebaseapp.com",
+  projectId: "peanutgo-c13f7",
+  storageBucket: "peanutgo-c13f7.appspot.com",
+  messagingSenderId: "390185139742",
+  appId: "1:390185139742:web:1f0f7d3296caba81ccf80f"
+
 };
+
 
 
 // Initialize Firebase
